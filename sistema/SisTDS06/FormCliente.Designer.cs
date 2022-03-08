d45073b6e16488217e9f6927261cfdb4c731dd30
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-03-07 22:11:34 -0300

    Aula 07/03/2022

---
+++ sistema/SisTDS06/FormCliente.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace SisTDS06
+{
+    partial class FormCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpaCampos = new System.Windows.Forms.Button();
+            this.btnCep = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.lblDtNascimento = new System.Windows.Forms.Label();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.lblCelular = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.dgvUsuario = new System.Windows.Forms.DataGridView();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.dtpDtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLimpaCampos
+            // 
+            this.btnLimpaCampos.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnLimpaCampos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpaCampos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLimpaCampos.Location = new System.Drawing.Point(577, 114);
+            this.btnLimpaCampos.Name = "btnLimpaCampos";
+            this.btnLimpaCampos.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpaCampos.TabIndex = 14;
+            this.btnLimpaCampos.Text = "Limpar";
+            this.btnLimpaCampos.UseVisualStyleBackColor = false;
+            this.btnLimpaCampos.Click += new System.EventHandler(this.btnLimpaCampos_Click);
+            // 
+            // btnCep
+            // 
+            this.btnCep.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCep.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCep.Location = new System.Drawing.Point(187, 177);
+            this.btnCep.Name = "btnCep";
+            this.btnCep.Size = new System.Drawing.Size(75, 23);
+            this.btnCep.TabIndex = 6;
+            this.btnCep.Text = "CEP";
+            this.btnCep.UseVisualStyleBackColor = false;
+            this.btnCep.Click += new System.EventHandler(this.btnCep_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSair.Location = new System.Drawing.Point(577, 143);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 15;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.Tomato;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Location = new System.Drawing.Point(577, 85);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 13;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Gold;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Location = new System.Drawing.Point(577, 56);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 12;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Location = new System.Drawing.Point(577, 27);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 11;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(272, 223);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(263, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(272, 205);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 61;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(16, 223);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.ReadOnly = true;
+            this.txtCidade.Size = new System.Drawing.Size(165, 20);
+            this.txtCidade.TabIndex = 9;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(16, 205);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(40, 13);
+            this.lblCidade.TabIndex = 57;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(501, 182);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.ReadOnly = true;
+            this.txtBairro.Size = new System.Drawing.Size(154, 20);
+            this.txtBairro.TabIndex = 8;
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(501, 164);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(37, 13);
+            this.lblBairro.TabIndex = 55;
+            this.lblBairro.Text = "Bairrro";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(272, 182);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.ReadOnly = true;
+            this.txtEndereco.Size = new System.Drawing.Size(212, 20);
+            this.txtEndereco.TabIndex = 7;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(272, 164);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(53, 13);
+            this.lblEndereco.TabIndex = 53;
+            this.lblEndereco.Text = "Endereço";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(16, 180);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(165, 20);
+            this.txtCep.TabIndex = 5;
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Location = new System.Drawing.Point(16, 162);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(28, 13);
+            this.lblCep.TabIndex = 51;
+            this.lblCep.Text = "CEP";
+            // 
+            // lblDtNascimento
+            // 
+            this.lblDtNascimento.AutoSize = true;
+            this.lblDtNascimento.Location = new System.Drawing.Point(194, 114);
+            this.lblDtNascimento.Name = "lblDtNascimento";
+            this.lblDtNascimento.Size = new System.Drawing.Size(104, 13);
+            this.lblDtNascimento.TabIndex = 48;
+            this.lblDtNascimento.Text = "Data de Nascimento";
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(16, 131);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(154, 20);
+            this.txtCelular.TabIndex = 3;
+            // 
+            // lblCelular
+            // 
+            this.lblCelular.AutoSize = true;
+            this.lblCelular.Location = new System.Drawing.Point(16, 113);
+            this.lblCelular.Name = "lblCelular";
+            this.lblCelular.Size = new System.Drawing.Size(39, 13);
+            this.lblCelular.TabIndex = 46;
+            this.lblCelular.Text = "Celular";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(378, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(16, 70);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 40;
+            this.lblNome.Text = "Nome";
+            // 
+            // dgvUsuario
+            // 
+            this.dgvUsuario.AllowUserToAddRows = false;
+            this.dgvUsuario.AllowUserToDeleteRows = false;
+            this.dgvUsuario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuario.Location = new System.Drawing.Point(13, 266);
+            this.dgvUsuario.Name = "dgvUsuario";
+            this.dgvUsuario.ReadOnly = true;
+            this.dgvUsuario.Size = new System.Drawing.Size(639, 174);
+            this.dgvUsuario.TabIndex = 39;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(16, 46);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 1;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(16, 28);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(27, 13);
+            this.lblCPF.TabIndex = 37;
+            this.lblCPF.Text = "CPF";
+            // 
+            // dtpDtNascimento
+            // 
+            this.dtpDtNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDtNascimento.Location = new System.Drawing.Point(194, 130);
+            this.dtpDtNascimento.Name = "dtpDtNascimento";
+            this.dtpDtNascimento.Size = new System.Drawing.Size(200, 20);
+            this.dtpDtNascimento.TabIndex = 4;
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnLocalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLocalizar.Location = new System.Drawing.Point(122, 44);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnLocalizar.TabIndex = 16;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = false;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // FormCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 450);
+            this.Controls.Add(this.btnLimpaCampos);
+            this.Controls.Add(this.btnCep);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.lblCep);
+            this.Controls.Add(this.lblDtNascimento);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.lblCelular);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.dgvUsuario);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.dtpDtNascimento);
+            this.Controls.Add(this.btnLocalizar);
+            this.Name = "FormCliente";
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.FormCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpaCampos;
+        private System.Windows.Forms.Button btnCep;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.Label lblDtNascimento;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label lblCelular;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.DataGridView dgvUsuario;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.DateTimePicker dtpDtNascimento;
+        private System.Windows.Forms.Button btnLocalizar;
+    }
+}
---
+++ sistema/SisTDS06/FormCliente.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SisTDS06
+{
+    public partial class FormCliente : Form
+    {
+        public FormCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCliente_Load(object sender, EventArgs e)
+        {
+             Cliente cli = new Cliente();
+             List<Cliente> cliente = cli.listacliente();
+             dgvUsuario.DataSource = cliente;
+        }
+
+        private void btnCep_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + txtCep.Text + "/json");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Servidor Indisponível!");
+                return; //Sai da rotina e para e codificação
+            }
+            using (Stream webStream = ChecaServidor.GetResponseStream())
+            {
+                if (webStream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webStream))
+                    {
+                        string response = responseReader.ReadToEnd();
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+
+                        String[] substrings = response.Split('\n');
+
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                if (valor[0] == "  erro")
+                                {
+                                    MessageBox.Show("CEP não encontrado!");
+                                    txtCep.Focus();
+                                    return;
+                                }
+                            }
+
+                            //Endereço
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                txtEndereco.Text = valor[1];
+                            }
+
+                            //Bairro
+                            if (cont == 4)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                txtBairro.Text = valor[1];
+                            }
+
+                            //Cidade
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                txtCidade.Text = valor[1];
+                            }
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            Cliente c = new Cliente();
+            c.Localiza(txtCPF.Text);
+            txtNome.Text = c.nome;
+            txtEndereco.Text = c.endereco;
+            txtEmail.Text = c.email;
+            txtBairro.Text = c.bairro;
+            txtCelular.Text = c.celular;
+            txtCidade.Text = c.cidade;
+            txtCep.Text = c.cep;
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Inserir(txtNome.Text, txtCelular.Text, dtpDtNascimento.Value, txtCep.Text, txtEndereco.Text, txtCidade.Text, txtBairro.Text, txtEmail.Text, txtCPF.Text);
+                List<Cliente> cliente = cli.listacliente();
+                MessageBox.Show("Cliente cadastrado com sucesso!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgvUsuario.DataSource = cliente;
+                txtNome.Text = "";
+                txtCelular.Text = "";
+                this.dtpDtNascimento.Value = DateTime.Now.Date;
+                txtCep.Text = "";
+                txtEndereco.Text = "";
+                txtCidade.Text = "";
+                txtBairro.Text = "";
+                txtEmail.Text = "";
+                txtCPF.Text = "";
+                ClassConecta.FecharConexao();
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message);
+            }
+            
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Atualizar(txtNome.Text, txtCelular.Text, dtpDtNascimento.Value, txtCep.Text, txtEndereco.Text, txtCidade.Text, txtBairro.Text, txtEmail.Text, txtCPF.Text);
+                List<Cliente> cliente = cli.listacliente();
+                MessageBox.Show("Cliente atualizado com sucesso!", "Edição", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgvUsuario.DataSource = cliente;
+                txtNome.Text = "";
+                txtCelular.Text = "";
+                this.dtpDtNascimento.Value = DateTime.Now.Date;
+                txtCep.Text = "";
+                txtEndereco.Text = "";
+                txtCidade.Text = "";
+                txtBairro.Text = "";
+                txtEmail.Text = "";
+                txtCPF.Text = "";
+                ClassConecta.FecharConexao();
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message);
+            }
+            
+           
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Exclui(txtCPF.Text);
+                List<Cliente> cliente = cli.listacliente();
+                MessageBox.Show("Usuário excluído com sucesso!", "Exclusão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgvUsuario.DataSource = cliente;
+                txtNome.Text = "";
+                txtCelular.Text = "";
+                this.dtpDtNascimento.Value = DateTime.Now.Date;
+                txtCep.Text = "";
+                txtEndereco.Text = "";
+                txtCidade.Text = "";
+                txtBairro.Text = "";
+                txtEmail.Text = "";
+                txtCPF.Text = "";
+                ClassConecta.FecharConexao();
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message);
+            }
+
+        }
+
+        private void btnLimpaCampos_Click(object sender, EventArgs e)
+        {
+
+            txtNome.Text = "";
+            txtCelular.Text = "";
+            this.dtpDtNascimento.Value = DateTime.Now.Date;
+            txtCep.Text = "";
+            txtEndereco.Text = "";
+            txtCidade.Text = "";
+            txtBairro.Text = "";
+            txtEmail.Text = "";
+            txtCPF.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
